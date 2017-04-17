e31694e79354539a4f60ef34550891549e23ac0b
arduinoteam20 <lancer856618@gmail.com>
2017-04-17 19:07:53 +0800

    Add files via upload

---
+++ checkpoint1/checkpoint1.ino
@@ -0,0 +1,31 @@
+void setup() {
+   pinMode(13,OUTPUT);
+   Serial.begin(9600);
+   Serial.println("Group:20");
+   Serial.println("04050192");
+   Serial.println("04050386");
+}
+void loop()
+{
+  char val;
+  if(Serial.available())
+  {
+    val = Serial.read(); 
+    switch(val)
+    {
+     
+      case'0':
+       digitalWrite(13,LOW);
+       Serial.println("LED OFF");
+       break;
+        case'1':
+        digitalWrite(13,HIGH);
+       Serial.println("LED ON");
+       break;
+       default:
+       digitalWrite(13,LOW);
+       Serial.println("error");
+      
+    }
+  }
+}
---
+++ checkpoint2/checkpoint2.ino
@@ -0,0 +1,40 @@
+#include <SevSeg.h>
+SevSeg sevseg; 
+void setup() {
+   Serial.begin(9600);
+   Serial.println("Group:20");
+   Serial.println("04050192");
+   Serial.println("04050386");
+
+  byte numDigits =4;
+   byte digitPins[] = {13,10,9,2};
+   byte segmentPins[] = {12,8,4,6,7,11,3,5};
+   sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+}
+void loop() 
+{
+ 
+  int a,n=0,x;
+    if (Serial.available()) 
+    { 
+         a = Serial.parseInt();
+         if(0<=a && a<=9999)
+          {
+            Serial.println(a);
+            sevseg.setNumber(a);
+            sevseg.refreshDisplay();
+          }
+     
+        else 
+          {
+            Serial.println("range error");
+          }
+      }   
+     else
+    sevseg.refreshDisplay();
+   
+   
+
+}
+
+
